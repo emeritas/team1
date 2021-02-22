6ddec2c311bab13fa8aecd6beb5c5601b5eee4ad
Aurimas <a.velyvis@blexr.com>
2021-02-22 21:59:12 +0200

    server preparation

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+// var bodyParser = require('body-parser');
+// var cors = require('cors')
+
+mongoose.connect('mongodb://localhost/reviews-db', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+})
+
+app.use('/api', routes);
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+
+
+router.get('/', (req, res) => {
+    res.json('Hello Team1')
+})
+
+
+module.exports = router
