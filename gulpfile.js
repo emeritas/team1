8c0b1c6ca7a4aba048017403ea703892c22998ec
airidas1 <airidas.zaliauskas@gmail.com>
2021-02-23 21:18:31 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var del = require('del');
 var runSequence = require('gulp4-run-sequence');
 
 // Basic Gulp task syntax
-gulp.task('hello', function() {
+gulp.task('hello', async function() {
   console.log('Hello Team1!');
 })
 
@@ -19,7 +19,7 @@ gulp.task('hello', function() {
 // -----------------
 
 // Start browserSync server
-gulp.task('browserSync', function() {
+gulp.task('browserSync', async function() {
   browserSync({
     server: {
       baseDir: 'app'
@@ -27,7 +27,7 @@ gulp.task('browserSync', function() {
   })
 })
 
-gulp.task('sass', function() {
+gulp.task('sass', async function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
     .pipe(gulp.dest('app/css')) // Outputs it in the css folder
@@ -37,7 +37,7 @@ gulp.task('sass', function() {
 })
 
 // Watchers
-gulp.task('watch', function() {
+gulp.task('watch', async function() {
   gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
   gulp.watch('app/*.html', browserSync.reload,);
   gulp.watch('app/js/**/*.js', browserSync.reload);
@@ -47,7 +47,7 @@ gulp.task('watch', function() {
 // ------------------
 
 // Optimizing CSS and JavaScript 
-gulp.task('useref', function() {
+gulp.task('useref', async function() {
 
   return gulp.src('app/*.html')
     .pipe(useref())
@@ -57,7 +57,7 @@ gulp.task('useref', function() {
 });
 
 // Optimizing Images 
-gulp.task('images', function() {
+gulp.task('images', async function() {
   return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
     // Caching images that ran through imagemin
     .pipe(cache(imagemin({
@@ -68,7 +68,7 @@ gulp.task('images', function() {
 
 
 // Cleaning 
-gulp.task('clean', function() {
+gulp.task('clean', async function() {
   return del.sync('dist').then(function(cb) {
     return cache.clearAll(cb);
   });
@@ -81,7 +81,7 @@ gulp.task('clean:dist', async function() {
 // Build Sequences
 // ---------------
 
-gulp.task('default', function(callback) {
+gulp.task('default', async function(callback) {
   runSequence(['sass', 'browserSync'], 'watch',
     callback
   )
