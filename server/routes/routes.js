4572e180090b04f4818369e6e50f117b432ff7e9
Aurimas <a.velyvis@blexr.com>
2021-02-24 21:55:53 +0200

    category model and controller

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -13,16 +13,16 @@ const upload = multer({
   storage
 })
 
-// Ka useris gales padaryti
 const UserController = require('../User/userController')
-// userio autentifikacija
 const UserMiddleware = require('../User/authenticate')
+const CategoryController = require('../Category/categoryController')
+
 
 router.get('/', (req, res) => {
     res.json('Hello Team1')
 })
 
-// userio registracija
+// User routes
 router.post('/signup', UserController.signUp)
 // userio prisijungimas
 router.post('/login', UserController.login)
@@ -31,8 +31,20 @@ router.get('/logout', UserMiddleware.authenticate, UserController.logout)
 // gauti visus useriui(testavimui)
 router.get('/users', UserController.getAllUsers)
 // atnaujinti userio descriptionui
-router.post('/user',UserMiddleware.authenticate, UserController.updateUserInfo)
+router.post('/user', UserMiddleware.authenticate, UserController.updateUserInfo)
 // failu ikelimas(NETIKRINAU AR VEIKIA NES NEZINAU KAIP DABAR PATIKRINT BE FE, KAI BUS FRONT ENDAS TADA TURBUT ZIURESIM)
 router.post('/uploads', UserMiddleware.authenticate, upload.single('test'), UserController.changePicture)
+
+
+
+
+
+
+
+// Category routes
+router.post('/category/add', CategoryController.addCategory)
+router.get('/category/get', CategoryController.getAll)
+router.patch('/category/update/:id', CategoryController.updateCategory)
+router.post('/category/delete/:id', CategoryController.deleteCategory)
 
 module.exports = router
