15b4b7ee24c3c74c9dd2449992e03f09f2a56a93
Aurimas <a.velyvis@blexr.com>
2021-02-28 18:41:01 +0200

    blog post adding

--- app/js/addReview.js
+++ app/js/addReview.js
@@ -12,9 +12,51 @@ async function getCategories() {
         headers:{"blog-user-id": webtok}
         })
         let categories = await response.json();
-        console.log(categories);
-        // document.getElementById('userAvatar').src = user.profileImageURL
+        let i = 0;
+        let option = document.createElement("option");
+        option.innerText = "Choose Category";
+        reviewCategory.append(option)
+        categories.forEach(category => {
+            i++;
+            if(i > 0){
+                option = document.createElement("option");
+                option.classList = "single-category";
+                option.innerText = category.title;
+                option.value = category._id;
+            }
+            reviewCategory.append(option)
+            
+        })
     }catch(e){
         console.log(e)
     }    
 }
+
+submit.addEventListener('click', async (e)=> {
+    e.preventDefault();
+    let title = reviewTitle.value;
+    let category = reviewCategory.options[reviewCategory.selectedIndex].value;
+    let content = reviewContent.value;
+
+    let data = {
+        title,
+        category,
+        content
+    }
+    try{
+        const response = await fetch('http://localhost:3001/api/blog', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'blog-user-id': webtok
+        },
+        body:JSON.stringify(data)
+        
+    })
+    if(response.status != 200) throw await response.json()
+    response.json()
+    }catch(e) {
+        console.log(e)
+    }
+    console.log(data)
+})
